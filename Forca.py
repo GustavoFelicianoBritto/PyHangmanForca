3d10310016db80a5ad6a8d1c1841239f6287065b
GustavoFelicianoBritto <gustavofelicianoemail@gmail.com>
2025-03-29 13:03:48 -0300

    Adicionada condições de vitórias e contador de vida

--- Forca.py
+++ Forca.py
@@ -8,26 +8,41 @@ wordList=["vaca","camelo","jabuti","ornitorrinco","paralelepipedo","elefante","t
 activeWord = random.choice(wordList)
 letterQtd = len(activeWord)
 acertos=0
+vidas=5
 
-print(f"Debug, a palavra é: {activeWord}")
+
+#print(f"Debug, a palavra é: {activeWord}")
+print(f"Bem vindo ao jogo da forca!!! \n"
+      f"Dica: Animal\n")
+
 wordGuess = ['_']*letterQtd
 
-while acertos != letterQtd:
+while acertos < letterQtd and vidas>0:
+
+    notValid = 0
 
     letter = str(input(f"{wordGuess}\nDigite uma letra: ").lower())
 
     for indexLetter, currentLetter in enumerate(activeWord):
         if letter == currentLetter:
-            print(f"Letra '{letter}' encontrada na posição {indexLetter}")
+            print(f"Letra '{letter}' encontrada na posição {indexLetter +1}")
             wordGuess[indexLetter] = letter
             acertos+=1
-        
+
+        else:
+            notValid+=1
+
+    if notValid == letterQtd:
+        vidas-=1
+        print("Opção incorreta")
 
 
+    print(f"Vidas restantes: {vidas}")
 
-
-
-
+if vidas <= 0:
+    print(f"Você perdeu! a palavra era: {activeWord}")
+else:
+     print(f"Parabéns, você acertou a palavra: {activeWord} com {vidas} vidas")
 
 
 print(wordGuess)
