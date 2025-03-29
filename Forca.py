baa9b3aa76b42461abf95c408c48a4d3faaa2aea
GustavoFelicianoBritto <gustavofelicianoemail@gmail.com>
2025-03-29 15:04:15 -0300

    Adicionadas categorias (Animais, Estados, Países)

--- Forca.py
+++ Forca.py
@@ -1,19 +1,54 @@
 import random
 
-wordList=["vaca","camelo","jabuti","ornitorrinco", "elefante","tartaruga","hipopotamo","rinoceronte","formiga",
+
+#Lista de palavras Animais, estados, objetos
+
+wordListAnimais =["vaca","camelo","jabuti","ornitorrinco", "elefante","tartaruga","hipopotamo","rinoceronte","formiga",
     "arara","capivara","tucano","lagarto","cachorro","papagaio","jacare","borboleta","girafa","pinguim","morcego","tigre","zebra",
     "macaco","golfinho","baleia","sapo","rã","peixe","piranha",
     "baiacu","aguia","falcão","gaivota","pelicano"]
 
-activeWord = random.choice(wordList)
+wordListEstados =["acre","alagoas","amapa","amazonas","bahia","ceara","goias","maranhao","paraiba","parana","pernambuco","piaui",
+"rondonia","roraima","sergipe","tocantins"]
+
+wordListCountry = ["argentina", "australia", "brazil", "canada", "chile",
+    "china", "colombia", "cuba", "egypt", "france",
+    "germany", "greece", "india", "israel", "italy",
+    "japan", "mexico", "norway", "poland", "russia"]
+
+categoria = "Animais"
+
+
+difficulty = int(input(f"Escolha o nível de dificuldade:\n1 - Animais (fácil)\n2 - Estados Brasileiros (intermediário)\n3 - Países em inglês (difícil): "))
+
+if(difficulty==1):
+    print("Opção fácil escolhida")
+    activeWord = random.choice(wordListAnimais)
+    categoria = "Animais"
+elif(difficulty==2):
+    print("Opção intermediária escolhida")
+    activeWord = random.choice(wordListEstados)
+    categoria = "Estados brasileiros"
+elif(difficulty==3):
+    print("Opção difícil escolhida")
+    activeWord = random.choice(wordListCountry)
+    categoria = "Países"
+else:
+    print("Opção inválida, categoria fácil escolhida")
+    activeWord = random.choice(wordListAnimais)
+    categoria = "Animais"
+
+
+
+#Quantidade de letra da palavra escolhida
 letterQtd = len(activeWord)
 acertos=0
 vidas=5
 
 
-#print(f"Debug, a palavra é: {activeWord}")
+print(f"Debug, a palavra é: {activeWord}")
 print(f"Bem vindo ao jogo da forca!!! \n"
-      f"Dica: Animal\n")
+      f"Dica: {categoria}\n")
 
 wordGuess = ['_']*letterQtd
 
